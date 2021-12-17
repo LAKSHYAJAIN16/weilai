2657162bf332f61e7e3d6475b40d54083fdeea37
LAKSHYAJAIN16 <81555595+LAKSHYAJAIN16@users.noreply.github.com>
2021-12-17 16:41:02 +0530

    Delete logic directory

--- logic/rarity-to-color.js
+++
@@ -1,9 +0,0 @@
-export default function rarityToColor(rarity) {
-  if (rarity === "Common") {
-    return "white";
-  } else if (rarity === "Uncommon") {
-    return "#e0ffe5";
-  } else if (rarity === "Rare") {
-    return "#bffaff";
-  }
-}
