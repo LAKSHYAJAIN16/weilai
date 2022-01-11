2b44fa5f5bf4cda9307d39f0f8d456f51ccbc65d
Lakshya Jain <81555595+LAKSHYAJAIN16@users.noreply.github.com>
2022-01-11 14:10:59 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import { UserProvider } from "../logic/userContext";
 import Timer from "../components/Timer";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6776078144980283"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <Component {...pageProps} />
-      <Timer ui={false}/>
+      <Timer ui={false} />
     </UserProvider>
   );
 }
